46c05dfa1e3820b94052a988ee28e0a3bc419a7f
adarshsingh55 <adarshsingh345125@gmail.com>
2023-06-29 08:03:50 +0530

    first update

--- src/component/noteGallery/NoteGallery.js
+++ src/component/noteGallery/NoteGallery.js
@@ -1,9 +1,74 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 import style from "./NoteGallery.module.css"
 function NoteGallery() {
+
+  const [tasks, setTasks] = useState([]);
+  const [inputValue, setInputValue] = useState('');
+  // console.log(tasks,"hello 1");
+
+  useEffect( () => {
+    const storedTasks =  localStorage.getItem('tasks');
+    // console.log(storedTasks,"hello 2");
+    if (storedTasks) {
+      setTasks(JSON.parse(storedTasks));
+      // console.log(tasks,"after set task");
+    }
+  }, []);
+
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  const handleAddTask = (e) => {
+    e.preventDefault();
+    if (inputValue.trim() !== '') {
+      const newTask = { task: inputValue, checked: false };
+      setTasks([...tasks, newTask]);
+      setInputValue('');
+      updateLocalStorage([...tasks, newTask]);
+    }
+  };
+
+  const handleRemoveTask = (index) => {
+    const updatedTasks = tasks.filter((_, i) => i !== index);
+    setTasks(updatedTasks);
+    updateLocalStorage(updatedTasks);
+  };
+
+  const handleToggleCheck = (index) => {
+    const updatedTasks = [...tasks];
+    updatedTasks[index].checked = !updatedTasks[index].checked;
+    setTasks(updatedTasks);
+    updateLocalStorage(updatedTasks);
+  };
+
+  const updateLocalStorage = (updatedTasks) => {
+    localStorage.setItem('tasks', JSON.stringify(updatedTasks));
+  };
+
   return (
     <div className={style.container}>
-      <input type="text" className={style.InputBox} />
+    <h1>Checklist</h1>
+    <div>
+      <form  onSubmit={ handleAddTask}>
+      <input type="text" value={inputValue} onChange={handleInputChange} />
+      <button type="submit">Add</button>
+      </form>
+    </div>
+      <ul>
+        {tasks.map((task, index) => (
+          <li key={index}>
+            <input
+              type="checkbox"
+              checked={task.checked}
+              onChange={() => handleToggleCheck(index)}
+            />
+            {task.task}
+            <button  onClick={() => handleRemoveTask(index)}>Remove</button>
+          </li>
+        ))}
+      </ul>
+   
     </div>
   )
 }
